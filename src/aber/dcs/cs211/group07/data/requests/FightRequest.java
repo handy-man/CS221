343da8e48687b568fbca53ce53fa31a280158e8f
Nathan Hand <administrator@thehiddennation.com>
2012-12-07 15:21:07 +0000

    fight and friend requests extend request and general interface additions

--- src/aber/dcs/cs211/group07/data/requests/FightRequest.java
+++ src/aber/dcs/cs211/group07/data/requests/FightRequest.java
@@ -2,7 +2,7 @@ package aber.dcs.cs211.group07.data.requests;
 
 import aber.dcs.cs211.group07.data.MonsterRefrence;
 
-public class FightRequest {
+public class FightRequest extends Request {
 
 	public MonsterRefrence toMonster;
 	public MonsterRefrence fromMonster;
