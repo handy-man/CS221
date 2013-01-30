c472566b712171f6fa627e748069855139e9764f
Sam Clements <sam@borntyping.co.uk>
2013-01-30 22:31:11 +0000

    Improved the way Fight.fight works and moved it to a better package

---
+++ src/packages/aber/dcs/cs211/group07/actions/Fight.java
@@ -0,0 +1,56 @@
+package aber.dcs.cs211.group07.actions;
+
+import java.util.Random;
+
+import aber.dcs.cs211.group07.data.Monster;
+
+public class Fight {
+	/**
+	 * Return a 'power' value for the monster
+	 */
+	public static double monsterPower(Monster monster){
+		Random r = new Random();
+		return monster.getStrength() + monster.getToughness() + r.nextDouble();
+	}
+	
+	/**
+	 * Make two monsters fight
+	 * 
+	 * @return The winning monster
+	 */
+	public static Monster fight(Monster monster1, Monster monster2) {
+		double monsterPower1 = Fight.monsterPower(monster1);
+		double monsterPower2 = Fight.monsterPower(monster2);
+		
+		Monster winner, loser;
+		double winnerPower, loserPower;
+		
+		/* Ensure there is a clear loser and winner, not a draw */
+		do {
+			if (monsterPower1 > monsterPower2) {
+				winner = monster1;
+				loser = monster2;
+				winnerPower = monsterPower1;
+				loserPower = monsterPower2;
+			} else {
+				winner = monster2;
+				loser = monster1;
+				winnerPower = monsterPower2;
+				loserPower = monsterPower1;
+			}
+		} while (monsterPower1 == monsterPower2);
+		
+		double powerDiffrence = 1.0 - (winnerPower - loserPower);
+		
+		/* Remove powerDiffrence from the winners health unless it is below 0 */
+		winner.increaseHealthLost(powerDiffrence < 0.0 ? 0.0 : powerDiffrence);
+		
+		/* Remove 1.0 from the losers health */
+		loser.increaseHealthLost(1.0);
+		
+		return winner;
+	}
+	
+	
+	
+}
--- src/packages/aber/dcs/cs211/group07/tests/FightTest.java
+++ src/packages/aber/dcs/cs211/group07/tests/FightTest.java
@@ -7,42 +7,30 @@ import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
 
-import aber.dcs.cs211.group07.data.Fight;
+import aber.dcs.cs211.group07.actions.Fight;
 import aber.dcs.cs211.group07.data.Monster;
+import aber.dcs.cs211.group07.data.Player;
 
 public class FightTest {
-
-	private Monster monster1;
-	private Monster monster2;
-	private Monster monster3;
-	private Fight fight;
-	
-	@Before
-	public void setup(){
-		final Date date = new Date();
-		monster1 = new Monster(1);
-		monster2 = new Monster(2);
-		monster3 = new Monster(1,1,"John", date, date,
-				0.1, 0.0, 1.0, 1.0, 1.0, 1, 1);
-		fight = new Fight();
-	}
+	final Player player = new Player(1, 0, "TestPlayer", "testpassword", 0);
 	
 	@Test
 	public void monsterPowerCalculationTest(){
-		assertTrue("Testing whether the monster power calculation is working correct",
-				fight.calculateMonsterPower(monster3) >= 0.0 &&
-				fight.calculateMonsterPower(monster3) <= 3.0 );
+		Monster m = new Monster(1, player,"John", new Date(), new Date(), 0.1, 0.0, 1.0, 1.0, 1.0, 1, 1);
+		double monsterPower = Fight.monsterPower(m);
+		assertTrue("The monster power is in within bounds", monsterPower >= 0.0 && monsterPower <= 3.0 );
 	}
 	
 	@Test
 	public void fightTest() {
-		assertTrue("Testing weather both monsters are on full health",
-				monster1.getHealthLost() == 0.0 &&
-				monster2.getHealthLost() == 0.0);
-		fight.fight(monster1, monster2);
-		assertTrue("Testing weather one of the monsters have lost health or died",
-				monster1.getHealthLost() != 0.0 ||
-				monster2.getHealthLost() != 0.0);
+		Monster monster1 = new Monster(player);
+		Monster monster2 = new Monster(player);
+		
+		assertTrue("Both monsters are on full health", monster1.health_lost == 0.0 && monster2.health_lost == 0.0);
+		
+		Fight.fight(monster1, monster2);
+		
+		assertTrue("One of the monsters is dead", monster1.health_lost == 1.0 || monster2.health_lost == 1.0);
 	}
 
 }
