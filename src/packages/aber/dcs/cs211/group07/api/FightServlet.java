b50ba68311eb3f088213e6f1b35facb11ef66e13
Dan Cornwell <dtcornwell@live.com>
2013-01-31 19:11:39 +0000

    Added servlets, need tiding tho
    dont have tomcat on my eclipse so cant see errors

---
+++ src/packages/aber/dcs/cs211/group07/api/FightServlet.java
@@ -0,0 +1,72 @@
+package aber.dcs.cs211.group07.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import aber.dcs.cs211.group07.actions.Fight;
+import aber.dcs.cs211.group07.data.Monster;
+import aber.dcs.cs211.group07.db.FriendsTableConnector;
+import aber.dcs.cs211.group07.db.MonsterTableConnector;
+import aber.dcs.cs211.group07.db.PlayerTableConnector;
+
+@WebServlet(name = "FriendshipRequest", urlPatterns = {"/fight/request"})
+public class FightServlet extends HttpServlet {
+
+	
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+ 
+    response.setContentType("text/plain;charset=UTF-8");
+	PrintWriter out = response.getWriter();
+    	
+	// Get a map of all the parameters set in the URL,
+	// including the parameter names and values
+	Map params = request.getParameterMap();
+ 
+	// We need to make sure all the parameters we need are set
+	// If not, send a HTTP error code
+	String[] requiredParams = new String[] {"fightID", "localMonsterID", "remoteMonsterID", "remoteServerNumber"};
+	if (!params.keySet().containsAll(Arrays.asList(requiredParams))) {
+	    response.sendError(
+		    HttpServletResponse.SC_BAD_REQUEST,
+		    "Parameter set incorrect");
+	} else {
+		MonsterTableConnector monsterTable = new MonsterTableConnector();
+		MonsterClient mon = new MonsterClient();
+		Monster ourMon = monsterTable.getMonster((int) params.get("localMonsterID"));
+		Monster theirMon = mon.getMonster((String)params.get("remoteMonsterID"), (int)params.get("remoteServerNumber"));
+		
+	    // Check that the localUserId actually exists on our server
+	    if (ourMon!=null && theirMon!=null) {
+	    	Fight fight = new Fight();
+	    	Monster winner = fight.fight(ourMon, theirMon);
+	    	
+	    	PlayerTableConnector playerTable = new PlayerTableConnector();
+	    	
+	    	if(winner==ourMon) {
+	    		//send lost request to them, update our stuff
+	    		playerTable.editMoney(ourMon.owner, 100);
+	    	}
+	    	else {
+	    		playerTable.editMoney(ourMon.owner, -100);
+	    		monsterTable.deleteMonster(ourMon);
+	    	}
+	    } 
+	    
+	    else {
+		response.sendError(
+			HttpServletResponse.SC_BAD_REQUEST,
+			"Local user ID does not exist");
+	    }
+	}
+	
+	}
+	
+}
+
---
+++ src/packages/aber/dcs/cs211/group07/api/FriendshipRequestServlet.java
@@ -0,0 +1,76 @@
+package aber.dcs.cs211.group07.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Map;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import aber.dcs.cs211.group07.db.FriendsTableConnector;
+import aber.dcs.cs211.group07.db.PlayerTableConnector;
+ 
+/**
+ * This is an example of a servlet which serves the /friends/request resource.
+ * Feel free to take this example and adapt it to your application's data model.
+ *
+ * @author James Bowcott
+ *
+ */
+@WebServlet(name = "FriendshipRequest", urlPatterns = {"/friends/request"})
+public class FriendshipRequestServlet extends HttpServlet {
+ 
+    @EJB
+ 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+ 
+    response.setContentType("text/plain;charset=UTF-8");
+	PrintWriter out = response.getWriter();
+    	
+	// Get a map of all the parameters set in the URL,
+	// including the parameter names and values
+	Map params = request.getParameterMap();
+ 
+	// We need to make sure all the parameters we need are set
+	// If not, send a HTTP error code
+	String[] requiredParams = new String[] {"friendID", "localUserID", "remoteUserID", "remoteServerID"};
+	if (!params.keySet().containsAll(Arrays.asList(requiredParams))) {
+	    response.sendError(
+		    HttpServletResponse.SC_BAD_REQUEST,
+		    "Parameter set incorrect");
+	} else {
+		PlayerTableConnector playerTable = new PlayerTableConnector();
+	    // Check that the localUserId actually exists on our server
+	    if (playerTable.getPlayer(Integer.parseInt((String) params.get("localUserID")))!=null) {
+	    	
+	    	FriendsTableConnector friendTable = new FriendsTableConnector();
+	    	if(friendTable.addFriend((String)params.get("friendID"),(String) params.get("localUserID"),(String) params.get("remoteUserID"),(String) params.get("removeServerID"))==true) {
+	    		//some sort of good response
+	    		JSONObject accept = new JSONObject();
+	    		accept.put("friendID", params.get("friendID"));
+	    		accept.write(out);
+	    	}
+	    	else {
+			    HttpServletResponse.SC_BAD_REQUEST,
+			    "Parameter set incorrect");
+	    	}
+	    } 
+	    
+	    else {
+		response.sendError(
+			HttpServletResponse.SC_BAD_REQUEST,
+			"Local user ID does not exist");
+	    }
+	}
+ 
+    }
+ 
+}
