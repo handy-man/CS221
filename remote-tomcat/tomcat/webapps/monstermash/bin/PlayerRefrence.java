11ea4323a466fd7765b5305a5293d918be689ef9
Nathan Hand <administrator@thehiddennation.com>
2013-02-18 10:32:36 +0000

    gitignore update, adding the class files

---
+++ remote-tomcat/tomcat/webapps/monstermash/bin/PlayerRefrence.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author adriangawryszewski
+ */
+public class PlayerRefrence implements Refrence {
+
+    public Player get() {
+        return null;
+    }
+}
+
---
+++ remote-tomcat/tomcat/webapps/monstermash/bin/Refrence.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Monster_classes;
+
+/**
+ *
+ * @author adriangawryszewski
+ */
+public interface Refrence {
+    
+}
